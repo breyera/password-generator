5006fd9f96afcdeb30cf1f9f87acb5902042750a
Alicia Breyer <alicia_breyer@yahoo.com>
2021-02-20 11:18:53 -0800

    fixed errors and finalized code

--- script.js
+++ script.js
@@ -11,19 +11,19 @@ function writePassword() {
 
 }
 
-// assign variables
-var lowercase = 'abcdefghijklmnopqrstuvwxyz'.split('');
-var capitalLetters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
-var numbers = '0123456789'.split('');
-var specialChar = '!"#$%&()*+,-./:;<=>?@[\]^_`{|}~'.split('');
-var lastArray = letters;
-var finalPassword = '';
-var x = 0;
 
 //create a function that prompts the user all the questions we need to ask
-function question() {
+function generatePassword() {
+  // assign variables
+  var lowercase = 'abcdefghijklmnopqrstuvwxyz'.split('');
+  var capitalLetters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
+  var numbers = '0123456789'.split('');
+  var specialChar = '!"#$%&()*+,-./:;<=>?@[\]^_`{|}~'.split('');
+  var finalPassword = '';
+  var finalArray = '';
+  var randomCharacter = 0;
   
-  var length = prompt('How long do you want your password to be?') 
+  var length = prompt('How long do you want your password to be?\n(8-128)') 
   /* loop to prompt them to choose a number, and also a number between 8 and 128 characters */
   while (isNaN(length)) { 
     alert('password length must be a number');
@@ -51,21 +51,36 @@ function question() {
   
    
     // start building password 
-    while (finalPassword.length<length) {
-      var randomCharacter 
+    while (finalPassword.length < length) {
+      // get random character set
+      randomCharacter = Math.floor(Math.random() * 4);
       // if they said yes to any of the choices above, randomly choose one of the arrays (from what they chose) (create an array from character type questions) and then randomly pick the index number to add to finalPassword
+      // check random character to choices above
+      if ( (randomCharacter === 0 && chooseLowercase) || (randomCharacter === 1 && chooseUppercase) || (randomCharacter === 2 && chooseNumbers) || (randomCharacter === 3 && chooseCharacters) ) {
 
-      Math.floor(Math.random() * //randomlychosenarray.length)
-      
-      finalPassword = finalPassword + randomCharacter
+        // generate random index number corresponding to character arrays - use switch
+        switch (randomCharacter) {
+          case 0 :  // for lowercase
+            finalArray = lowercase[Math.floor(Math.random() * lowercase.length)];
+            break;
+            case 1 : // for uppercase
+            finalArray = capitalLetters[Math.floor(Math.random() * capitalLetters.length)];
+            break;
+            case 2 : // choose number
+            ffinalArray = numbers[Math.floor(Math.random() * numbers.length)];
+            break;
+            case 3 : // choose special character
+            finalArray = specialChar[Math.floor(Math.random() * specialChar.length)];
+            break;
+          }
+        //add character to final password string
+        finalPassword += finalArray;
+      }
+
 
 
     }
-
-  //create object to store all user input 
-  //retun object that was created
+    return finalPassword;
 }
 
-
-
-question()
+generateBtn.addEventListener('click', writePassword);
