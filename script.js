644812652c2a3982d306d9db3eb6cb860b01cd3d
Alicia Breyer <alicia_breyer@yahoo.com>
2021-02-19 23:48:34 -0800

    added variables and added alert

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 // Assignment Code
 
-//create arrays for each character type
-
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
@@ -13,15 +11,21 @@ function writePassword() {
 
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+// assign variables
+var lowercase = 'abcdefghijklmnopqrstuvwxyz'.split('');
+var capitalLetters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
+var numbers = '0123456789'.split('');
+var specialChar = '!"#$%&()*+,-./:;<=>?@[\]^_`{|}~'.split('');
+var lastArray = letters;
+var finalPassword = '';
+var x = 0;
 
 //create a function that prompts the user all the questions we need to ask
 function question() {
   //if else statement, with >8 return back to beginning, if <128 return back
-  var length=prompt('How long do you want your password to be?') 
+  var length = prompt('How long do you want your password to be?') 
 
-  if (length < 8 || length >128) {
+  if (length < 8 || length > 128) {
     alert('password must between 8 and 128 characters');
     return;
   } 
@@ -31,10 +35,19 @@ function question() {
     return;
   }
 
-  //prompt questions asking about each character type (use confirm instead of prompt)
+  //prompt questions asking about each character type 
+  var chooseLowercase = confirm('Would you like to include lowercase letters?');
+  var chooseUppercase = confirm('Would you like to include uppercase letters?');
+  var chooseNumbers = confirm('Would you like to include numbers?');
+  var chooseCharacters = confirm('Would you like to include special characters?');
   //if statement to see if they said yes(true) to at least one of the four questions - if no(false), alert must select at least one character type and return to beginning of questions
+  if(!chooseLowercase && !chooseUppercase && !chooseNumbers && !chooseCharacters) {
+    alert('You must choose at least one character type from the previous questions');
+  } else {
+    
+  }
 
-  //create object to store all user input
+  //create object to store all user input 
   //retun object that was created
 }
 
